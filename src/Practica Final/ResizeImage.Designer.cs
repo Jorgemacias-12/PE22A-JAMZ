a554c7a80bfa25a12b310ea438ee67eceebe9c6f
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-05-11 10:54:40 -0500

    Agregando archivos de codigo, diseño y recursos
    	Este es un desarrollo paralelo a forma de segunda opción por si
    	la primera práctica es mal recibida.

---
+++ src/Practica Final/ResizeImage.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PE22A_JAMZ.src.TabRenderer
+{
+    partial class ResizeImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlContainer = new System.Windows.Forms.Panel();
+            this.PnlSideBar = new System.Windows.Forms.Panel();
+            this.LblHeight = new System.Windows.Forms.Label();
+            this.LblWidth = new System.Windows.Forms.Label();
+            this.TxtHeight = new System.Windows.Forms.TextBox();
+            this.LblQuantity = new System.Windows.Forms.Label();
+            this.BtnSelectImages = new System.Windows.Forms.Button();
+            this.CbxAspectRatio = new System.Windows.Forms.CheckBox();
+            this.LblFormat = new System.Windows.Forms.Label();
+            this.CbxFormat = new System.Windows.Forms.ComboBox();
+            this.TxtWidth = new System.Windows.Forms.TextBox();
+            this.FlpImages = new System.Windows.Forms.FlowLayoutPanel();
+            this.LblInfo = new System.Windows.Forms.Label();
+            this.PnlContainer.SuspendLayout();
+            this.PnlSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlContainer
+            // 
+            this.PnlContainer.Controls.Add(this.PnlSideBar);
+            this.PnlContainer.Controls.Add(this.FlpImages);
+            this.PnlContainer.Controls.Add(this.LblInfo);
+            this.PnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlContainer.Location = new System.Drawing.Point(0, 0);
+            this.PnlContainer.Name = "PnlContainer";
+            this.PnlContainer.Size = new System.Drawing.Size(1671, 763);
+            this.PnlContainer.TabIndex = 0;
+            // 
+            // PnlSideBar
+            // 
+            this.PnlSideBar.Controls.Add(this.LblHeight);
+            this.PnlSideBar.Controls.Add(this.LblWidth);
+            this.PnlSideBar.Controls.Add(this.TxtHeight);
+            this.PnlSideBar.Controls.Add(this.LblQuantity);
+            this.PnlSideBar.Controls.Add(this.BtnSelectImages);
+            this.PnlSideBar.Controls.Add(this.CbxAspectRatio);
+            this.PnlSideBar.Controls.Add(this.LblFormat);
+            this.PnlSideBar.Controls.Add(this.CbxFormat);
+            this.PnlSideBar.Controls.Add(this.TxtWidth);
+            this.PnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlSideBar.Location = new System.Drawing.Point(0, 39);
+            this.PnlSideBar.Name = "PnlSideBar";
+            this.PnlSideBar.Size = new System.Drawing.Size(273, 724);
+            this.PnlSideBar.TabIndex = 11;
+            // 
+            // LblHeight
+            // 
+            this.LblHeight.AutoSize = true;
+            this.LblHeight.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblHeight.Location = new System.Drawing.Point(12, 110);
+            this.LblHeight.Name = "LblHeight";
+            this.LblHeight.Size = new System.Drawing.Size(41, 19);
+            this.LblHeight.TabIndex = 2;
+            this.LblHeight.Text = "Alto:";
+            // 
+            // LblWidth
+            // 
+            this.LblWidth.AutoSize = true;
+            this.LblWidth.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblWidth.Location = new System.Drawing.Point(12, 70);
+            this.LblWidth.Name = "LblWidth";
+            this.LblWidth.Size = new System.Drawing.Size(58, 19);
+            this.LblWidth.TabIndex = 1;
+            this.LblWidth.Text = "Ancho:";
+            // 
+            // TxtHeight
+            // 
+            this.TxtHeight.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtHeight.Location = new System.Drawing.Point(59, 105);
+            this.TxtHeight.Name = "TxtHeight";
+            this.TxtHeight.Size = new System.Drawing.Size(201, 29);
+            this.TxtHeight.TabIndex = 4;
+            this.TxtHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Validation_KeyPress);
+            // 
+            // LblQuantity
+            // 
+            this.LblQuantity.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblQuantity.Location = new System.Drawing.Point(12, 216);
+            this.LblQuantity.Name = "LblQuantity";
+            this.LblQuantity.Size = new System.Drawing.Size(248, 19);
+            this.LblQuantity.TabIndex = 10;
+            this.LblQuantity.Text = "Imagen(es) seleccionada(s):";
+            // 
+            // BtnSelectImages
+            // 
+            this.BtnSelectImages.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(215)))), ((int)(((byte)(96)))));
+            this.BtnSelectImages.FlatAppearance.BorderSize = 0;
+            this.BtnSelectImages.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSelectImages.Location = new System.Drawing.Point(12, 20);
+            this.BtnSelectImages.Name = "BtnSelectImages";
+            this.BtnSelectImages.Size = new System.Drawing.Size(248, 35);
+            this.BtnSelectImages.TabIndex = 8;
+            this.BtnSelectImages.Text = "Escoger imagen(es)";
+            this.BtnSelectImages.UseVisualStyleBackColor = false;
+            this.BtnSelectImages.Click += new System.EventHandler(this.BtnSelectImages_Click);
+            // 
+            // CbxAspectRatio
+            // 
+            this.CbxAspectRatio.AutoSize = true;
+            this.CbxAspectRatio.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CbxAspectRatio.Location = new System.Drawing.Point(12, 148);
+            this.CbxAspectRatio.Name = "CbxAspectRatio";
+            this.CbxAspectRatio.Size = new System.Drawing.Size(185, 18);
+            this.CbxAspectRatio.TabIndex = 5;
+            this.CbxAspectRatio.Text = "Mantener relación de aspecto";
+            this.CbxAspectRatio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CbxAspectRatio.UseVisualStyleBackColor = true;
+            // 
+            // LblFormat
+            // 
+            this.LblFormat.AutoSize = true;
+            this.LblFormat.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblFormat.Location = new System.Drawing.Point(12, 178);
+            this.LblFormat.Name = "LblFormat";
+            this.LblFormat.Size = new System.Drawing.Size(73, 19);
+            this.LblFormat.TabIndex = 6;
+            this.LblFormat.Text = "Formato:";
+            // 
+            // CbxFormat
+            // 
+            this.CbxFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbxFormat.FormattingEnabled = true;
+            this.CbxFormat.Items.AddRange(new object[] {
+            "PNG",
+            "JPG"});
+            this.CbxFormat.Location = new System.Drawing.Point(91, 178);
+            this.CbxFormat.Name = "CbxFormat";
+            this.CbxFormat.Size = new System.Drawing.Size(169, 23);
+            this.CbxFormat.TabIndex = 7;
+            // 
+            // TxtWidth
+            // 
+            this.TxtWidth.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtWidth.Location = new System.Drawing.Point(76, 65);
+            this.TxtWidth.Name = "TxtWidth";
+            this.TxtWidth.Size = new System.Drawing.Size(184, 29);
+            this.TxtWidth.TabIndex = 3;
+            this.TxtWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Validation_KeyPress);
+            // 
+            // FlpImages
+            // 
+            this.FlpImages.AutoScroll = true;
+            this.FlpImages.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.FlpImages.BackColor = System.Drawing.Color.White;
+            this.FlpImages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FlpImages.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.FlpImages.Location = new System.Drawing.Point(0, 39);
+            this.FlpImages.Name = "FlpImages";
+            this.FlpImages.Padding = new System.Windows.Forms.Padding(0, 10, 30, 0);
+            this.FlpImages.Size = new System.Drawing.Size(1671, 724);
+            this.FlpImages.TabIndex = 0;
+            // 
+            // LblInfo
+            // 
+            this.LblInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LblInfo.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblInfo.Location = new System.Drawing.Point(0, 0);
+            this.LblInfo.Name = "LblInfo";
+            this.LblInfo.Size = new System.Drawing.Size(1671, 39);
+            this.LblInfo.TabIndex = 0;
+            this.LblInfo.Text = "Redimensionar imagen(es)";
+            this.LblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResizeImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1671, 763);
+            this.Controls.Add(this.PnlContainer);
+            this.Name = "ResizeImage";
+            this.Text = "ResizeImage";
+            this.PnlContainer.ResumeLayout(false);
+            this.PnlSideBar.ResumeLayout(false);
+            this.PnlSideBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlContainer;
+        private System.Windows.Forms.Label LblInfo;
+        private System.Windows.Forms.Label LblHeight;
+        private System.Windows.Forms.Label LblWidth;
+        private System.Windows.Forms.TextBox TxtWidth;
+        private System.Windows.Forms.TextBox TxtHeight;
+        private System.Windows.Forms.CheckBox CbxAspectRatio;
+        private System.Windows.Forms.Label LblFormat;
+        private System.Windows.Forms.ComboBox CbxFormat;
+        private System.Windows.Forms.Button BtnSelectImages;
+        private System.Windows.Forms.FlowLayoutPanel FlpImages;
+        private System.Windows.Forms.Label LblQuantity;
+        private System.Windows.Forms.Panel PnlSideBar;
+    }
+}
---
+++ src/Practica Final/ResizeImage.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PE22A_JAMZ.src.TabRenderer
+{
+    public partial class ResizeImage : Form
+    {
+        public ResizeImage()
+        {
+            InitializeComponent();
+        }
+
+        bool IsAspectRatio;
+        private int ImageWidth;
+        private int ImageHeight;
+        private int QuantityOfImages;
+        private string[] FileNames;
+        private string ImageFormat;
+
+        private OpenFileDialog OpenDialog;
+        private SaveFileDialog SaveDialog;
+        private DialogResult Result;
+
+
+        private async void SelectImages()
+        {
+            OpenDialog = new OpenFileDialog();
+
+            OpenDialog.InitialDirectory = @"C:\";
+            OpenDialog.Title = "Escoge imagen(es) a redimensionar";
+            OpenDialog.RestoreDirectory = true;
+            OpenDialog.Multiselect = true;
+
+            if (ImageFormat == "jpg") OpenDialog.Filter = "JPG Image(.jpg)|*.jpg";
+            if (ImageFormat == "png") OpenDialog.Filter = "Png Image(.png)|*.png";
+
+            Result = OpenDialog.ShowDialog();
+
+            if (Result == DialogResult.Cancel ||
+                Result == DialogResult.None ||
+                Result == DialogResult.No ||
+                Result == DialogResult.Abort)
+            {
+                return;
+            }
+
+            if (Result == DialogResult.OK)
+            {
+
+                QuantityOfImages = OpenDialog.FileNames.Length;
+
+                FileNames = new string[QuantityOfImages];
+
+                for (int i = 0; i < QuantityOfImages; i++)
+                {
+                    FileNames[i] = OpenDialog.FileNames[i];
+                }
+
+            }
+
+            LblQuantity.Text += $" {QuantityOfImages}";
+
+            await ImagePreview();
+        }
+
+        private Task ImagePreview()
+        {
+
+            FlpImages.Controls.Clear();
+
+            PictureBox SelectedImage;
+
+            for (int i = 0; i < QuantityOfImages; i++)
+            {
+                SelectedImage = new PictureBox();
+                SelectedImage.Size = new Size(400, 300);
+                SelectedImage.BackColor = Color.Black;
+                SelectedImage.BackgroundImage = Image.FromFile(FileNames[i]);
+                SelectedImage.BackgroundImageLayout = ImageLayout.Stretch;
+                SelectedImage.Margin = new Padding(10, 10, 10, 10);
+                SelectedImage.Update();
+
+                FlpImages.Controls.Add(SelectedImage);
+                FlpImages.Update();
+
+            }
+
+            return Task.CompletedTask;
+
+        }
+
+        private void ResetUi()
+        {
+            LblQuantity.Text = "Imagen(es) seleccionada(s):";
+        }
+
+        private void ValidateInput()
+        {
+
+            if (TxtWidth.Text.Equals(""))
+            {
+                MessageBox.Show("El campo esta vació.",
+                                "¡Atención!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                TxtWidth.Focus();
+                return;
+            }
+
+            if (TxtHeight.Text.Equals(""))
+            {
+                MessageBox.Show("El campo esta vació.",
+                                "¡Atención!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                TxtHeight.Focus();
+                return;
+            }
+
+            if (CbxFormat.SelectedIndex == -1)
+            {
+                MessageBox.Show("Escoge un formato de imagen", 
+                                "¡Atención!", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Warning);
+                return;
+            }
+
+            IsAspectRatio = CbxAspectRatio.Checked;
+            ImageWidth = Int32.Parse(TxtWidth.Text);
+            ImageHeight = Int32.Parse(TxtHeight.Text);
+            ImageFormat = CbxFormat.Text.ToLower();
+
+            SelectImages();
+            
+        }
+
+        private async void BtnSelectImages_Click(object sender, EventArgs e)
+        {
+            ResetUi();
+            ValidateInput();    
+        }
+
+        private void Validation_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            TextBox Field = sender as TextBox;
+
+            if (!char.IsControl(e.KeyChar) &&
+                !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && 
+                (Field.Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+            if (Field.Text.Length > 4)
+            {
+                e.Handled = true;
+            }
+
+
+        }
+    }
+}
